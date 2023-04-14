56720b5ccc5ebb92a282bce2ac01015c5ef2eecf
w <857613798@qq.com>
2023-04-14 19:31:17 +0800

    修改样式

--- miniprogram/app.ts
+++ miniprogram/app.ts
@@ -8,11 +8,37 @@ App<IAppOption>({
     wx.setStorageSync('logs', logs)
 
     // 登录
-    wx.login({
-      success: res => {
-        console.log(res.code)
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      },
-    })
+    // wx.login({
+    //   success: res => {
+    //     console.log(res.code)
+    //     // 发送 res.code 到后台换取 openId, sessionKey, unionId
+    //   },
+    // })
+
+    // wx.getSetting({
+    //   success(res) {
+    //     if (!res.authSetting['scope.record']) { 
+    //       wx.authorize({ 
+    //         scope: 'scope.record', success() { } 
+    //       })
+    //     }
+    //   }
+    // })
+
+    // wx.showModal({
+    //   title: '提示',
+    //   content: '当前未登录，是否登录',
+    //   showCancel: true,
+    //   success (res) {
+    //     if (res.confirm) {
+          
+    //     } else if (res.cancel) {
+    //       wx.showToast({
+    //         icon: 'none',
+    //         title: '取消登录'
+    //       })
+    //     }
+    //   }
+    // })
   },
 })
--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -4,9 +4,12 @@ const app = getApp<IAppOption>()
 
 Page({
   data: {
+    dialog_show: false,
     showOptions: false,
-    languageOptions:[],
+    select_pop: false,
+    languageOptions:['英语','中文'],
     nowLanguage:"英语",
+    selectIndex: 0,
     inputBut:true,
     motto: '欢迎使用Ai人工智能翻译',
     userInfo: {},
@@ -15,6 +18,44 @@ Page({
     canIUseGetUserProfile: false,
     canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
   },
+
+  // 点击选择语言，显示弹出层
+  onVoiceInput(){
+    this.setData({
+      select_pop: true
+    })
+  },
+  // 关闭弹出层
+  close_pop(){
+    this.setData({
+      select_pop: false
+    })
+  },
+  // 弹出层确定
+  onConfirm(e){
+    let {index} = e.detail
+    this.setData({
+      select_pop: false,
+      selectIndex: index
+    })
+  },
+  // 弹出层取消
+  onCancel(){
+    this.setData({
+      select_pop: false
+    })
+  },
+
+  // 语音录入
+  speech_input(){
+    console.log(6666);
+  },
+
+  // 点击翻译
+  translate(){
+
+  },
+
   // 事件处理函数
   bindViewTap() {
     wx.navigateTo({
@@ -29,12 +70,26 @@ Page({
       })
     }
   },
+  onShow(){
+    let userInfo = wx.getStorageSync('userInfo')
+    console.log(userInfo);
+    if(!userInfo){
+      this.setData({
+        dialog_show: true
+      })
+    }
+    
+  },
   getUserProfile() {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    this.setData({
+      dialog_show: false,
+    })
     wx.getUserProfile({
       desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
       success: (res) => {
         console.log(res)
+        wx.setStorageSync('userInfo',res.userInfo)
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
